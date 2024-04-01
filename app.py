9ac92f48e93657322bbae05e4cf2a1e8f9ef5f0d
m2zhang <marilyn2zhang@gmail.com>
2024-04-01 09:30:00 -0400

    for login

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
